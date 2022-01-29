67ada98db4f36c471b2b9f83455151334c0227c3
delucis <swithinbank@gmail.com>
2022-01-29 11:01:17 +0100

    Use the Netlify site URL if set in env

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 // Full Astro Configuration API Documentation:
 // https://docs.astro.build/reference/configuration-reference
 
-// @type-check enabled!
-// VSCode and other TypeScript-enabled text editors will provide auto-completion,
-// helpful tooltips, and warnings if your exported object is invalid.
-// You can disable this by removing "@ts-check" and `@type` comments below.
-
 // @ts-check
 export default /** @type {import('astro').AstroUserConfig} */ ({
-	// Enable the React renderer to support React JSX components.
-	renderers: ['@astrojs/renderer-react'],
+  // Enable the React renderer to support React JSX components.
+  renderers: ['@astrojs/renderer-react'],
+
+  buildOptions: {
+    // Use the URL provided by Netlify when building there. See:
+    // https://docs.netlify.com/configure-builds/environment-variables/#deploy-urls-and-metadata
+    site: process.env.URL,
+  },
 });
