9b0d92e26d7123d8519651fc4fa5d7f3c7d6fbf7
delucis <swithinbank@gmail.com>
2022-03-28 01:00:57 +0200

    Inject Netlify Identity logic with the integration
    Avoids scattering various logic around the templates unnecessarily. One note: no support for injecting only on specific pages from what I can tell.

--- integrations/netlify-cms.ts
+++ integrations/netlify-cms.ts
@@ -5,6 +5,21 @@ export default function NetlifyCMS() {
   const NetlifyCMSIntegration: AstroIntegration = {
     name: 'netlify-cms',
     hooks: {
+      'astro:config:setup': ({ injectScript }) => {
+        // Add Netlify Identity widget & login logic to the page bundle.
+        injectScript(
+          'page',
+          `import identity from 'netlify-identity-widget';
+          identity.on("init", user => {
+            if (!user) {
+              identity.on("login", () => {
+                document.location.href = "/admin/";
+              });
+            }
+          });
+          identity.init();`
+        );
+      },
     },
   };
   return [react(), NetlifyCMSIntegration];
