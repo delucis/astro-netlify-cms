f5b06ed24ec3f90ed17a6dd33def80e531e9ffd3
delucis <swithinbank@gmail.com>
2022-09-02 13:00:51 +0200

    Run `netlify-cms-proxy-server` in shell on Windows
    Fix #13
    
    Co-Authored-By: preetamslot <5420582+preetamslot@users.noreply.github.com>

--- integration/index.ts
+++ integration/index.ts
@@ -48,7 +48,11 @@ export default function NetlifyCMS({
         );
       },
       'astro:server:start': () => {
-        const proxy = spawn('netlify-cms-proxy-server', { stdio: 'inherit' });
+        const proxy = spawn('netlify-cms-proxy-server', {
+          stdio: 'inherit',
+          // Run in shell on Windows to make sure the npm package can be found.
+          shell: process.platform === 'win32',
+        });
         process.on('exit', () => proxy.kill());
       },
     },
