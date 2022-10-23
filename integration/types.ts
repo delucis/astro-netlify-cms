6757440b968332f0b1dc6a52ee70a6c1852f7b15
Chris Swithinbank <swithinbank@gmail.com>
2022-10-24 00:26:14 +0200

    Use `injectRoute` to inject admin dashboard (#30)

--- integration/types.ts
+++ integration/types.ts
@@ -1,5 +1,14 @@
+import type CMS from 'netlify-cms-app';
+import type { CmsConfig } from 'netlify-cms-core';
+
 export type NormalizedPreviewStyle =
   | [pathOrUrl: string]
   | [rawCSS: string, meta: { raw: boolean }];
 
 export type PreviewStyle = string | NormalizedPreviewStyle;
+
+export interface InitCmsOptions {
+  cms: typeof CMS;
+  config: CmsConfig;
+  previewStyles?: NormalizedPreviewStyle[];
+}
